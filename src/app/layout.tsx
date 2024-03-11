b6ac2eb170221609784aa76115d106bb7b8cb361
ImSreyas <sreyassatheesh755@gmail.com>
2024-03-12 00:00:13 +0530

    coin test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
 
 export const metadata: Metadata = {
   title: "Duem",
+  description: "Online ledger"
 };
 
 export default function RootLayout({
@@ -15,7 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="container" ></div>
+        {children}
+      </body>
     </html>
   );
 }
