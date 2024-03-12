d2bea6cfcefdadb7ada3d6721c43b4b55802e6f1
ImSreyas <sreyassatheesh755@gmail.com>
2024-03-12 23:19:57 +0530

    Nav icons added.

--- src/app/Nav.tsx
+++ src/app/Nav.tsx
@@ -1,37 +1,40 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import subSrc from "/public/icons/sun.svg";
+import notificationSrc from "/public/icons/bell.svg";
+import profileSrc from "/public/icons/user.svg";
 
 const Nav = () => {
   return (
     <nav className="flex justify-between items-center px-20 py-8 h-fit">
       <div></div>
       <div className="flex justify-center items-center">
-        <div className="border border-neutral-700 ms-3 h-11 w-11 rounded-full">
-          {/* <Image src="" alt="" className="h-full w-full" /> */}
+        <div className="cursor-pointer flex justify-center items-center border border-neutral-700 ms-3 h-12 w-12 rounded-full">
+          <Image src={subSrc} alt="darkMode" width={10} height={10} className="text-white h-5 w-5" />
         </div>
-        <div className="border border-neutral-700 mx-3 h-11 w-11 rounded-full">
-          {/* <Image src="" alt="" className="h-full w-full" /> */}
+        <div className="cursor-pointer flex justify-center items-center border border-neutral-700 mx-3 h-12 w-12 rounded-full">
+          <Image src={notificationSrc} alt="" width={10} height={10} className="h-5 w-5" />
         </div>
         <div className="border rounded-xl h-fit w-fit border-neutral-700">
           <Link
             href="/"
-            className="inline-block px-6 py-2 border-e border-neutral-700"
+            className="inline-block px-9 py-2.5 border-e border-neutral-700"
           >
             Collections
           </Link>
           <Link
             href="/history"
-            className="inline-block px-6 py-2 border-e border-neutral-700"
+            className="inline-block px-9 py-2.5 border-e border-neutral-700"
           >
             History
           </Link>
-          <Link href="/dashboard" className="inline-block px-6 py-2">
+          <Link href="/dashboard" className="inline-block px-9 py-2.5">
             Dashboard
           </Link>
         </div>
-        <div className="border border-neutral-700 ms-3 h-11 w-11 rounded-2xl">
-          {/* <Image src="" alt="" className="h-full w-full" /> */}
+        <div className="flex justify-center items-center border border-neutral-700 ms-3 h-12 w-12 rounded-full">
+          <Image src={profileSrc} alt="profile" width={10} height={10} className="text-white h-5 w-5" />
         </div>
       </div>
     </nav>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={poppins.className}>
         <Nav />
         {children}
       </body>
