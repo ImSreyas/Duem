828e6bd806d0b40dbbd79deed0a13f7071c2bb55
ImSreyas <sreyassatheesh755@gmail.com>
2024-04-16 11:16:41 +0530

    new host ip added.

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     // Modify the webpack config to set the default dev server options
     if (dev && isServer) {
       config.devServer = {
-        host: '192.168.75.58', // Set the IP address
+        host: '192.168.190.58', // Set the IP address
         port: 3000, // Set the port number
         open: true, // Open the default browser when the server starts
       };
